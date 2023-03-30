66e8a74776d7007704313d8465e18065b7ca713b
HAMIMI dany <dany.hamimi@gmail.com>
2023-03-30 12:35:58 +0200

    Update config.py

--- config.py
+++ config.py
@@ -10,11 +10,13 @@ has2Hands = False
 
 valFinded = -2
 # Create an AITrain object
+mnist = tf.keras.datasets.mnist
+(x_train, y_train), (x_test, y_test) = (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
+x_train = tf.keras.utils.normalize(x_train, axis=1)
+x_test = tf.keras.utils.normalize(x_test, axis=1)
 model = tf.keras.models.load_model('number.model')
-#modelBis = tf.keras.models.load_model('mnist.h5')
-modelBis = tf.keras.models.load_model('emnist_letters.h5')
-
+modelBis = tf.keras.models.load_model('mnist.h5')
 
 cap = cv2.VideoCapture(0)
 
@@ -53,9 +55,6 @@ buttonValFinded = pygame.transform.scale(butFindedBis, (500, 70))
 
     #Add buttons to the window
 
-
-def index_to_letter(index):
-    return chr(index + ord('A'))
 
 def init():
 
